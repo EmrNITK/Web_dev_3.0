4cf02e30015287671c81813758966f1cb94dabad
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-09 02:09:36 +0530

    Add Teams.router.js

---
+++ backend/routes/Teams.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getAllTeams, getTeamById, createTeam, updateTeam } from '../controllers/Teams.controller.js';
+import { verifyJwt } from '../middlewares/auth.middlewares.js';
+
+const teamRouter = express.Router();
+
+teamRouter.get('/', verifyJwt, getAllTeams);
+teamRouter.get('/:teamId', verifyJwt, getTeamById);
+teamRouter.post('/', verifyJwt, createTeam);
+teamRouter.put('/:teamId/', verifyJwt, updateTeam);
+
+
+export default teamRouter;
