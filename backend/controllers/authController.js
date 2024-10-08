edc851e46a31ff8be3882a80f5e64224d2f7efe1
shivam <rajshivam0432@gmail.com>
2024-10-08 21:42:42 +0530

    Add the auth.middlewares.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,9 @@
 import bcrypt from "bcryptjs";
 import asyncHandler from "../utils/asyncHandler.js";
 import User from "../models/User.model.js";
+import { createToken } from "../utils/jwt.js";
 
-export const register = asyncHandler(async (req, res) => {
+export const register = asyncHandler(async (req, res,next) => {
   const isPresent = await User.findOne({ email: req.body.email });
 
   if (isPresent) {
@@ -23,5 +24,31 @@ export const register = asyncHandler(async (req, res) => {
   const registerUser = await user.save();
   res.status(200).json(registerUser);
 });
-export const login = () => {};
-export const logout = () => {};
+export const login = asyncHandler(async (req, res,next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(401).json({ message: "User not found" });
+  }
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+  const token = createToken(user.id, user.email);
+  console.log("here")
+  res
+    .status(200)
+    .cookie("jwt", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 604800000,
+    })
+    .json({ message: "logged in successfully" });
+    console.log('here too')
+});
+export const logout = (req,res) => {
+  res.clearCookie('jwt');
+  res.status(200).json({"message":"logged out succesfully"});
+
+};
