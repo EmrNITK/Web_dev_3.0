896f02ab0180effabbea6a4242d60fec56461a75
shivam <rajshivam0432@gmail.com>
2024-10-08 19:58:39 +0530

    Add validateRegistration middleware

--- backend/routes/Auth.routes.js
+++ backend/routes/Auth.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {register,logout,login} from '../controllers/authController.js';
+import { register, logout, login } from "../controllers/authController.js";
+import { validateRegistration } from "../middlewares/validate.js";
 const authRouter = express.Router();
 
-authRouter.post('/register',register);
+authRouter.post("/register", validateRegistration, register);
 
-authRouter.post('/login',login);
+authRouter.post("/login", login);
 
-authRouter.post('/logout',logout);
-
+authRouter.post("/logout", logout);
 
 export default authRouter;
