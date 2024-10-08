3f3efb3bb42cf21526ba7575f3cfd2d3bdb24a01
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-09 01:15:02 +0530

    Remove redundant field id

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    id: {
-      type: mongoose.Schema.Types.ObjectId,
-      auto: true,
-    },
     name: {
       type: String,
       required: true,
