a3c8e4ba4d9267d13b2a1b2bb8607ba11038da2d
shivam <rajshivam0432@gmail.com>
2024-10-08 19:59:04 +0530

    Implement /register controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,27 @@
 import bcrypt from "bcryptjs";
-import { asyncHandler } from "../utils/asyncHandler";
+import asyncHandler from "../utils/asyncHandler.js";
+import User from "../models/User.model.js";
 
-export const register = (req, res) => {
-    
-};
+export const register = asyncHandler(async (req, res) => {
+  const isPresent = await User.findOne({ email: req.body.email });
+
+  if (isPresent) {
+    res.status(409).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const user = await User.create({
+    name: req.body.name,
+    branch: req.body.branch,
+    collegeName: req.body.collegeName,
+    mobileNo: req.body.mobileNo,
+    rollNo: req.body.rollno,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+
+  const registerUser = await user.save();
+  res.status(200).json(registerUser);
+});
 export const login = () => {};
 export const logout = () => {};
