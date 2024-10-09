ca976212e1df783a85cc5501e95aaa2119b93b87
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-09 23:07:57 +0530

    Implement removeMember method

--- backend/controllers/Member.controller.js
+++ backend/controllers/Member.controller.js
@@ -1,11 +1,25 @@
 import Team from "../models/Team.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
-const removeMember = asyncHandler(async(req,res)=>{
-    const {memberId,teamId} = req.params;
+const removeMember = asyncHandler(async (req, res) => {
+    const { memberId, teamId } = req.params;
 
     const team = await Team.findById(teamId);
-    const index = team.members.findIndex((id)=>id == memberId);
 
-    console.log(index);
+    if (!team) {
+        res.status(404).json({ message: "Team doesn't exists" });
+    }
+
+    const index = team.members.findIndex((id) => id == memberId);
+
+    if (index == -1) {
+        res.status(404).json({ message: "Team member not found" });
+    }
+
+    team.members.splice(index, 1);
+
+    await team.save();
+    res.status(200).json({ message: "Member removed successfully" });
 });
+
+export default removeMember;
