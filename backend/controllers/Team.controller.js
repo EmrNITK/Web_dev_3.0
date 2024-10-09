4db4c4682ff1a922e3444d497365bf673279a62d
akshita119 <akshita119akshita119@gmail.com>
2024-10-10 02:24:56 +0530

    Add controllers for JoinTeamRequest, rejectTeamRequest and acceptTeamRequest

--- backend/controllers/Team.controller.js
+++ backend/controllers/Team.controller.js
@@ -59,3 +59,73 @@ export const updateTeam = asyncHandler(async (req, res) => {
 
 });
 
+export const JoinTeamRequest = asyncHandler(async (req, res) => {
+    const teamId = req.params.teamId;
+  
+    const userId = req.userId;
+  
+    const user = await User.findById(userId);
+  
+    const team = await Team.findById(teamId);
+  
+    const leaderId = team.leader.toString();
+    const userr = await User.findById(leaderId);
+  
+    await sendjoinRequestEmail(user, userr);
+  
+    res.status(200).json({ message: "join request sent" });
+  });
+  export const acceptTeamRequest = asyncHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const teamId = req.params.teamId;
+  
+    const userExists = await User.findById(userId);
+    if (!userExists) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
+  
+    const updatedTeam = await Team.findByIdAndUpdate(
+      teamId,
+      { $addToSet: { members: userId } },
+      { new: true, runValidators: true }
+    );
+  
+    if (!updatedTeam) {
+      return res.status(404).json({ success: false, message: "Team not found" });
+    }
+    const leaderId = updatedTeam.leader.toString();
+    const leader = await User.findById(leaderId);
+  
+    if (updatedTeam.members.length > 4) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Cannot exceed 4 members" });
+    }
+    await sendJoinAcceptanceEmail(userExists, updatedTeam, leader);
+    res.status(200).json({ success: true, data: updatedTeam });
+  
+    console.log("updateTeam", updateTeam);
+    // res.status(200).json({ message: "memeberis added" });
+  });
+  export const rejectTeamRequest = asyncHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const teamId = req.params.teamId;
+  
+    const userExists = await User.findById(userId);
+    if (!userExists) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
+  
+    const updatedTeam = await Team.findByIdAndUpdate(teamId);
+  
+    if (!updatedTeam) {
+      return res.status(404).json({ success: false, message: "Team not found" });
+    }
+    const leaderId = updatedTeam.leader.toString();
+    const leader = await User.findById(leaderId);
+  
+    await sendJoinRejectionEmail(userExists, updatedTeam, leader);
+    res.status(200).json({ success: true, msg: "rejected request" });
+  
+    console.log("updateTeam", updateTeam);
+  });
