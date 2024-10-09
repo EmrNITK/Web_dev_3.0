b8710021385219a35d1be2b0a5c89f6feb5c4592
akshita119 <akshita119akshita119@gmail.com>
2024-10-10 02:22:37 +0530

    Add getVerifiedUser and getUserById

--- backend/routes/User.router.js
+++ backend/routes/User.router.js
@@ -3,6 +3,8 @@ import {
   sendMailToAdmin,
   rejectedInviteByAdmin,
  acceptedInviteByAdmin,
+ getVerifiedUser,
+ getUserById
 } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
 
@@ -13,5 +15,9 @@ userRouter.post("/verify", verifyJwt, sendMailToAdmin);
 userRouter.put("/verify/accept", verifyJwt, acceptedInviteByAdmin);
 
 userRouter.post("/verify/reject", verifyJwt, rejectedInviteByAdmin);
+
+userRouter.get("/", verifyJwt, getVerifiedUser);
+
+userRouter.get("/:userId", verifyJwt, getUserById);
 
 export default userRouter;
