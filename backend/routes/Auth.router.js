1e064fd219e58d521e1df71a35b390234ae0d8d1
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-10 00:24:27 +0530

    Add forgot_password routes

--- backend/routes/Auth.router.js
+++ backend/routes/Auth.router.js
@@ -13,4 +13,7 @@ authRouter.post("/login", validateLogin, login);
 
 authRouter.post("/logout", logout);
 
+authRouter.post("/forgot_password/get_otp",sendOTP);
+authRouter.post("/forgot_password/verify",verifyOTP);
+
 export default authRouter;
