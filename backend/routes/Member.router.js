de1dfaa4b064da8fdac280e3586d92ba33f6463f
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-10 13:20:23 +0530

    Fix route

--- backend/routes/Member.router.js
+++ backend/routes/Member.router.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import removeMember from '../controllers/Member.controller.js';
+import { verifyJwt } from '../middlewares/auth.middlewares.js';
 
 const memberRouter = express.Router();
 
-memberRouter.delete('/:memberId',removeMember);
+memberRouter.delete('/:teamId/members/:memberId',verifyJwt, removeMember);
 
 export default memberRouter;
