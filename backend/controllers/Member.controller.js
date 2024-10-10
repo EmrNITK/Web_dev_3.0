edfb719b2b88623c96305e533e55fe70be772651
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-10 10:46:49 +0530

    Update member doucment in db

--- backend/controllers/Member.controller.js
+++ backend/controllers/Member.controller.js
@@ -10,15 +10,22 @@ const removeMember = asyncHandler(async (req, res) => {
         res.status(404).json({ message: "Team doesn't exists" });
     }
 
-    const index = team.members.findIndex((id) => id == memberId);
+    const member = await User.findById(memberId);
+    if(!member) {
+        res.status(404).json({message:"Member doesn't exists"});
+    }
 
+    const index = team.members.findIndex((id) => id == memberId);
     if (index == -1) {
         res.status(404).json({ message: "Team member not found" });
     }
 
     team.members.splice(index, 1);
-
     await team.save();
+
+    member.teamId = null;
+    await member.save();
+    
     res.status(200).json({ message: "Member removed successfully" });
 });
 
