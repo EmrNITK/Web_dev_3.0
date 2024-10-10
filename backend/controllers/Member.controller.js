79fae5f4f8a6757eeda41a27801dc4a86f961774
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-10 13:20:13 +0530

    Update to verify only leader can remove and if memer is in team or not

--- backend/controllers/Member.controller.js
+++ backend/controllers/Member.controller.js
@@ -1,31 +1,46 @@
-import Team from "../models/Team.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { sendRemoveEmail } from "../utils/sendMail.js";
+import Team from '../models/Team.model.js';
+import User from '../models/User.model.js';
 
 const removeMember = asyncHandler(async (req, res) => {
     const { memberId, teamId } = req.params;
+    const userId = req.userId;
 
+    // Check if team exists
     const team = await Team.findById(teamId);
-
     if (!team) {
-        res.status(404).json({ message: "Team doesn't exists" });
+        return res.status(404).json({ message: "Team doesn't exists" });
     }
 
+    const leader = await User.findById(team.leader);
+    // Check if user is leader
+    if (team.leader.toString() != userId) {
+        return res.status(401).json({ message: "Only leader can remove members" });
+    }
+
+    // Check if member exists
     const member = await User.findById(memberId);
-    if(!member) {
-        res.status(404).json({message:"Member doesn't exists"});
+    if (!member) {
+        return res.status(404).json({ message: "Member doesn't exists" });
     }
 
-    const index = team.members.findIndex((id) => id == memberId);
+    // Check if member is in team
+    const index = team.members.findIndex(member => member.toString() === memberId.toString());
     if (index == -1) {
-        res.status(404).json({ message: "Team member not found" });
+        return res.status(404).json({ message: "Team member not found" });
     }
 
+    // Remove member from team
     team.members.splice(index, 1);
     await team.save();
 
+    // Update member
     member.teamId = null;
     await member.save();
-    
+
+    // Send mail to member
+    await sendRemoveEmail(member, team, leader);
     res.status(200).json({ message: "Member removed successfully" });
 });
 
