08ea457cbc301f1fd299f417a804713cd5b1349f
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-10 15:45:48 +0530

    Add some error handling and edge cases

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,71 +1,67 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import User from "../models/User.model.js";
-import { sendVerificationEmail,sendAcceptanceEmail,sendRejectionEmail } from "../utils/sendMail.js";
+import { sendVerificationEmail, sendAcceptanceEmail, sendRejectionEmail } from "../utils/sendMail.js";
 
 export const sendMailToAdmin = asyncHandler(async (req, res) => {
   const userId = req.userId;
-  console.log(userId);
-  const { transaction_id } = req.body;
-  console.log(transaction_id, userId);
+  const { transactionId } = req.body;
+
   const user = await User.findById(userId);
-  console.log(user);
-  //   const adminEmail = process.env.ADMIN_EMAIL ;
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
 
-  await sendVerificationEmail(user, transaction_id);
+  await sendVerificationEmail(user, transactionId);
 
   res.status(200).json({ message: "Verification email sent successfully" });
 });
 
-export const acceptedInviteByAdmin = asyncHandler(async (req, res) => {
-  const userId = req.userId;
-  console.log(userId);
+export const acceptVerification = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
 
   const user = await User.findById(userId);
-  console.log(user)
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
+
+  if (user.isVerified) {
+    return res.status(403).json({ message: "User already verified" });
+  }
+
   await User.findByIdAndUpdate(userId, { isVerified: true });
   await sendAcceptanceEmail(user);
 
-  console.log("verified mail has been sent ")
-  res.json({ message: `User ${userId} has been verified.` });
+  res.json({ message: `User verified successfully` });
 });
 
-export const rejectedInviteByAdmin = asyncHandler(async (req, res) => {
-  const userId = req.userId;
-  console.log(userId);
+export const rejectVerification = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
 
-  const user = await User.findOne({ id: userId });
+  const user = await User.findOne({ _id: userId });
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
- 
+
   await sendRejectionEmail(user);
 
-  console.log("verified mail has been sent ")
-
-  res.status(403).json({ message: `User ${userId} has been rejected.` });
+  res.status(403).json({ message: `User has been rejected.` });
 });
 
-export const getVerifiedUser =asyncHandler(async(req,res)=>{
-  const user=await User.find({});
-  console.log(user);
+export const getVerifiedUser = asyncHandler(async (req, res) => {
+  const user = await User.find({});
+  
   const verifiedUsers = user.filter(users => users.isVerified === true);
 
   res.status(200).json(verifiedUsers);
 });
 
-export const getUserById =asyncHandler(async(req,res)=>{
-  const UserId=req.params.userId;
-  const user=await User.findById(UserId);
-  if(!user){
-    return res.status(404).json({success:"false",msg:"user not found"})
+export const getUserById = asyncHandler(async (req, res) => {
+  const userId = req.params.userId;
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" })
   }
-  console.log(UserId);
+  
   res.status(200).json(user);
 });
 
