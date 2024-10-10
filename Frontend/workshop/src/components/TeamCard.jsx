c14e4729b95db238ff4e36c2adc5b7111d4472b6
supremeashu <ashutoshmohapatra03@gmail.com>
2024-10-10 17:55:27 +0530

    Add Vite configuration and React setup also included join team age

---
+++ Frontend/workshop/src/components/TeamCard.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+
+const TeamCard = ({ team, handleJoin }) => {
+	const [message, setMessage] = useState("");
+
+	return (
+		<div className="bg-white p-3 rounded-lg shadow-lg">
+			<div className="flex flex-row justify-between">
+				<h3 className="text-lg font-semibold text-red-500 mb-2">
+					{team.name}
+				</h3>
+				<div className="mb-4">
+					<h4 className=" text-sm font-semibold text-black">Team Leader email:</h4>
+					{team.members.length === 0 ? (
+						<p className="text-gray-500">No members yet.</p>
+					) : (
+						<ul className="text-sm list-disc list-inside text-black ">
+							{team.leader.email}
+						</ul>
+					)}
+				</div>
+				<p className="text-sm text-gray-600 mb-4">Members: {team.members.length} / 4</p>
+				{message && (
+					<div className="bg-green-100 text-green-700 p-2 rounded mb-2">
+						{message}
+					</div>
+				)}
+				<button
+					onClick={() => handleJoin(team.name)}
+					className={`text-sm h-10 px-4 rounded-3xl text-white ${
+						team.members.length >= 4
+							? "bg-gray-400 cursor-not-allowed"
+							: "bg-blue-500 hover:bg-blue-600"
+					} transition duration-200`}
+					disabled={team.members.length >= 4 || team.joinRequestPending}
+				>
+					{team.members.length >= 4 ? "Full" : "Join"}
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default TeamCard;
---
+++ Frontend/workshop/src/components/TeamList.jsx
@@ -0,0 +1,28 @@
+// src/components/TeamList.js
+import React from "react";
+import TeamCard from "./TeamCard";
+
+const TeamList = ({ teams, handleJoin }) => {
+	const availableTeams = teams.filter((team) => team.members.length < 4);
+
+	return (
+		<div className="w-screen h-full bg-blue-950 px-10">
+			<h2 className="text-4xl font-bold my-6 text-center text-white">
+				Available Teams to Join
+			</h2>
+			{availableTeams.length === 0 ? (
+				<p className="text-center text-xl text-gray-400 ">
+					No available teams to join. You can create one!
+				</p>
+			) : (
+				<div className="flex flex-col gap-4 flex-grow">
+					{availableTeams.map((team) => (
+						<TeamCard key={team.id} team={team} handleJoin={handleJoin} />
+					))}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default TeamList;
