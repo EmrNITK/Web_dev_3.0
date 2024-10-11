7b8f14c7b72fb06e30de92dc7746fc3ebcbc5ff5
supremeashu <ashutoshmohapatra03@gmail.com>
2024-10-11 09:41:14 +0530

    Add main website frontend, including join team page routes

---
+++ Frontend/src/pages/TeamPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+// import FacultyCard from "../components/Faculty/FacultyCard"
+import Card from '../components/Team/Card'
+import Card2 from '../components/Team/Card2'
+import NavBar2 from '../components/Navbar2';
+import {
+    StarsCanvas,
+  
+  } from "../components";
+
+
+function TeamPage() {
+    return (
+
+
+        
+        // <div>
+        // {/* <   div className='title'>Faculty InCharge</div> */}
+        //         {/* <FacultyCard /> */}
+        //     <div className='title'>Present Team</div>
+        //         <Card />
+        //         <StarsCanvas/>
+        // </div>
+
+<div>
+
+        <NavBar2 />
+        <div className="relative z-0 bg-primary">
+            <Card2 />
+            <Card />
+        {/* <Navbar /> */}
+          {/* <UpcomingWorkshops /> */}
+          {/* <Contact /> */}
+
+          <StarsCanvas />
+        </div>
+</div>
+
+    );
+}
+
+export default TeamPage;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./pages/createTeam.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
