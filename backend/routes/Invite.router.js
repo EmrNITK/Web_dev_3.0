aaf74b358d4f1a7e85dba4692a9e81c0493ff680
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-16 05:29:04 +0530

    Update routes to GET

--- backend/routes/Invite.router.js
+++ backend/routes/Invite.router.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { verifyJwt } from '../middlewares/auth.middlewares.js';
-import { sendInvitations, acceptedInviteResponse, rejectedInviteResponse } from '../controllers/Invite.controller.js';
+import { sendInvitations, acceptInvitation, rejectInvitation } from '../controllers/Invite.controller.js';
 
 
 const inviteRouter = express.Router();
 
 // invitation routes
 inviteRouter.post('/:teamId', verifyJwt, sendInvitations);
-inviteRouter.put('/:teamId/invite/:userId/accept', acceptedInviteResponse);
-inviteRouter.post('/:teamId/invite/:userId/reject', rejectedInviteResponse);
+inviteRouter.get('/:teamId/invite/:userId/accept', acceptInvitation);
+inviteRouter.get('/:teamId/invite/:userId/reject', rejectInvitation);
 
 export default inviteRouter;
