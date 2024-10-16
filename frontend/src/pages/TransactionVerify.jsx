83a1f513b026ee9a50839f45b242004ae5b1efa0
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-16 05:45:42 +0530

    Fix error handling

--- frontend/src/pages/TransactionVerify.jsx
+++ frontend/src/pages/TransactionVerify.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { verifyUser } from "../api/apiService";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-import Header from "../components/Header"
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+
 const VerifyTransaction = () => {
   const [transactionId, setTransactionId] = useState("");
   const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
 
   const handleVerify = async () => {
     setError("");
@@ -15,28 +15,17 @@ const VerifyTransaction = () => {
 
     // Input validation
     if (!transactionId.trim()) {
-      setError("Transaction ID cannot be empty.");
+      setError("Please enter Transaction ID");
       setLoading(false);
       return;
     }
 
     try {
-      // const userData = { transactionId };
       const response = await verifyUser(transactionId);
-
-      console.log("Verifying transaction ID:", transactionId);
-
-      if (response && response.ok) {
-        navigate("/workshop/createteam");
-      } else {
-        setError(
-          response?.message ||
-            "Verification failed. Please check the Transaction ID."
-        );
-      }
-    } catch (err) {
-      console.error(err);
-      setError(err.message || "An error occurred during verification.");
+      setMessage(`${response.message} Wait for verification from admin`);
+    } catch (error) {
+      console.error(error);
+      setError(error.message || "An error occurred during verification.");
     } finally {
       setLoading(false);
       setTransactionId("");
@@ -46,28 +35,45 @@ const VerifyTransaction = () => {
   return (
     <div>
       <Header />
-    <div className="flex items-center justify-center h-screen ">
-      <div className="p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-2xl font-bold text-white text-center mb-6">
-          Verify Transaction
-        </h2>
-        <input
-          type="text"
-          placeholder="Enter Transaction ID"
-          value={transactionId}
-          onChange={(e) => setTransactionId(e.target.value)}
-          className="w-full p-2 mb-4 rounded-md border border-gray-600 bg-gray-700 text-white"
-        />
-        <button
-          onClick={handleVerify}
-          className="w-full p-2 bg-green-500 hover:bg-green-600 text-white rounded-md"
-          disabled={loading}
-        >
-          {loading ? "Verifying..." : "Verify"}
-        </button>
-        {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+      <div className="flex items-center justify-center h-screen ">
+        <div className="p-12 rounded-lg shadow-lg max-w-md w-full bg-white/5  backdrop-opacity-5 backdrop-brightness-10 shadow-lg backdrop-blur-sm rounded-lg ">
+          <h2 className="text-2xl font-bold text-white text-center mb-6">
+            Verify Transaction
+          </h2>
+          <input
+            type="text"
+            placeholder="Enter Transaction ID"
+            value={transactionId}
+            onChange={(e) => setTransactionId(e.target.value)}
+            className="w-full p-2 mb-4 rounded-md border border-gray-600 bg-gray-700 text-white"
+          />
+          <button
+            onClick={handleVerify}
+            className="w-full p-2 bg-green-500 hover:bg-green-600 text-white rounded-md"
+            disabled={loading}
+          >
+            {loading ? "Verifying..." : "Verify"}
+          </button>
+          {error && (
+            <p className="mt-4 font-mono text-red-500 text-center text-sm">{error}</p>
+          )}
+          {message && (
+            <div className="text-center">
+              <p className="mt-4 font-mono text-green-500 text-center text-sm">
+                {message}
+              </p>
+              <div className="center font-mono  ">
+                <Link
+                  to="/workshop"
+                  className="text-blue-500 hover:text-white text-sm underline"
+                >
+                  Go to Workshop Page
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
