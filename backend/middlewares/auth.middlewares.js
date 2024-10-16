2d541e2d74cb1d717342219e93cf9eb2dc5eeda6
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-16 16:30:41 +0530

    Update token retrival statement

--- backend/middlewares/auth.middlewares.js
+++ backend/middlewares/auth.middlewares.js
@@ -4,25 +4,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const verifyJwt = asyncHandler(async (req, res,next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ message: "unauthorised" });
   }
 
   const decodedtoken = jwt.verify(token, process.env.SECRET);
- 
+
   req.userId = decodedtoken.id;
 
   next();
 });
 
-export const verifyTempOtpJwt = asyncHandler(async (req, res,next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
+export const verifyTempOtpJwt = asyncHandler(async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-     return  res.status(401).json({ message: "unauthorised" });
+    return res.status(401).json({ message: "unauthorised" });
   }
 
   const decodedtoken = jwt.verify(token, process.env.SECRET);
