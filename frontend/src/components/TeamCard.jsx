fb86f2e02645585e2eb0749b2d335154f320c3d0
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-16 05:48:35 +0530

    Fix issues

--- frontend/src/components/TeamCard.jsx
+++ frontend/src/components/TeamCard.jsx
@@ -1,46 +1,36 @@
-import React, { useState } from "react";
-
+import React from "react";
 
 const TeamCard = ({ team, handleJoin }) => {
-	const [message, setMessage] = useState("");
-	console.log("TEAM",team)
-
-	return (
-		<div className=" col-span-4 grid grid-cols-subgrid justify-items-start bg-white/5  backdrop-opacity-5 backdrop-brightness-10 shadow-lg backdrop-blur-sm p-3 rounded-lg shadow-lg">
-			
-				<h3 className="text-lg font-semibold text-white-500 mb-2">
-					{team.name}
-				</h3>
-				<div className="mb-4">
-					<h4 className=" text-sm  text-white-300">Team Leader email:</h4>
-					{team.members.length === 0 ? (
-						<p className="text-gray-800">No members yet.</p>
-					) : (
-						<ul className="text-sm list-disc list-inside text-gray-600  ">
-							{team.leader?.email}
-						</ul>
-					)}
-				</div>
-				<p className="text-sm text-white-750 mb-4 justify-self-center">Members: {team.members.length} / 4</p>
-				{message && (
-					<div className="bg-green-100 text-green-700 p-2 rounded mb-2">
-						{message}
-					</div>
-				)}
-				<button
-					onClick={() => handleJoin(team._id)}
-					className={`text-sm h-10 px-4 rounded-3xl text-white ${
-						team.members.length >= 4
-							? "bg-gray-400 cursor-not-allowed"
-							: "bg-blue-500 hover:bg-blue-600"
-					} transition duration-200 justify-self-center`}
-					disabled={team.members.length >= 4 || team.joinRequestPending}
-				>
-					{team.members.length >= 4 ? "Full" : "Join"}
-				</button>
-			
-		</div>
-	);
+  return (
+    <div className=" grid place-items-start lg:col-span-4  lg:grid-cols-subgrid lg:content-center lg:place-items-center lg:justify-items-center bg-white/5  backdrop-opacity-5 backdrop-brightness-10 shadow-lg backdrop-blur-sm p-3 rounded-lg shadow-lg">
+      <div className="text-lg font-semibold text-white-500 mb-2 lg:mb-0 l">
+        {team.name}
+      </div>
+      <div className="mb-4 lg:mb-0 ">
+        {team.members.length === 0 ? (
+          <p className="text-gray-800">No members yet.</p>
+        ) : (
+          <ul className="text-sm list-disc list-inside text-gray-400  ">
+            {team.leader?.email}
+          </ul>
+        )}
+      </div>
+      <p className="font-sans text-sm text-white-750 mb-4 lg:mb-0">
+        Members: {team.members.length} / 4
+      </p>
+      <button
+        onClick={() => handleJoin(team._id)}
+        className={`text-sm px-4 py-2 rounded-full text-white ${
+          team.members.length >= 4
+            ? "bg-gray-400 cursor-not-allowed"
+            : "bg-blue-500 hover:bg-blue-600"
+        } transition duration-200`}
+        disabled={team.members.length >= 4 || team.joinRequestPending}
+      >
+        {team.members.length >= 4 ? "Full" : "Join"}
+      </button>
+    </div>
+  );
 };
 
 export default TeamCard;
