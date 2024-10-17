0e08835b94e06a0a60c5ae53a7c45327846bffdf
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-17 18:48:00 +0530

    Change some styles

--- frontend/src/components/TeamCard.jsx
+++ frontend/src/components/TeamCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const TeamCard = ({ team, handleJoin }) => {
   return (
     <div className=" grid place-items-start lg:col-span-4  lg:grid-cols-subgrid lg:content-center lg:place-items-center lg:justify-items-center bg-white/5  backdrop-opacity-5 backdrop-brightness-10 shadow-lg backdrop-blur-sm p-3 rounded-lg shadow-lg">
-      <div className="text-lg font-semibold text-white-500 mb-2 lg:mb-0 l">
+      <div className="text-lg justify-self-start font-semibold text-white-500 mb-2 lg:mb-0 l">
         {team.name}
       </div>
-      <div className="mb-4 lg:mb-0 ">
+      <div className="mb-4 lg:mb-0 justify-self-start">
         {team.members.length === 0 ? (
           <p className="text-gray-800">No members yet.</p>
         ) : (
@@ -20,7 +20,7 @@ const TeamCard = ({ team, handleJoin }) => {
       </p>
       <button
         onClick={() => handleJoin(team._id)}
-        className={`text-sm px-4 py-2 rounded-full text-white ${
+        className={`justify-self-end md:justify-self-center text-sm px-4 py-2 rounded-full text-white ${
           team.members.length >= 4
             ? "bg-gray-400 cursor-not-allowed"
             : "bg-blue-500 hover:bg-blue-600"
