00897325cee291abc311d1e2ad0884073f28f0d1
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-17 13:06:08 +0530

    Update UI and improve error handling

--- frontend/src/pages/TeamDetails.jsx
+++ frontend/src/pages/TeamDetails.jsx
@@ -7,7 +7,8 @@ import { Link } from "react-router-dom";
 const TeamDetails = () => {
   const { user, updateUser } = useContext(AuthContext);
   const [team, setTeam] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [fetching, setFetching] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
 
@@ -20,29 +21,55 @@ const TeamDetails = () => {
           setTeam(team);
         }
       } catch (err) {
-        setError(err.message || "Error fetching team details");
+        console.log(err);
       } finally {
-        setLoading(false);
+        setFetching(false);
       }
     };
 
     fetchTeamDetails();
   }, []);
 
-  const handleDelete = async (teamId, _id) => {
-    setTeam((prevTeam) => ({
-      ...prevTeam,
-      members: prevTeam.members.filter((member) => member._id !== _id),
-    }));
-    removeMember(teamId)
-  }
-  const handleDeleteTeam = async (teamId) => {
-    console.log(teamId)
-    deleteTeam(teamId)
-  }
+  const handleDeleteMember = async (memberId) => {
+    const teamId = user?.teamId?._id;
+    try {
+      setLoading(true);
+      const response = await removeMember(teamId, memberId);
+      setTeam((prevTeam) => ({
+        ...prevTeam,
+        members: prevTeam.members.filter((member) => member._id !== memberId),
+      }));
+      setMessage(response.message);
+      setError("");
+    } catch (error) {
+      console.error(error);
+      setError(error.message || "An error occurred during deleting member.");
+      setMessage("");
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  const handleDeleteTeam = async () => {
+    const teamId = user?.teamId?._id;
+    try {
+      setLoading(true);
+      const response = await deleteTeam(teamId);
+      setTeam(null);
+      setMessage(response.message);
+      setError("");
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      setError(error.message || "An error occurred during deleting team.");
+      setMessage("");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  if (loading)
+  if (fetching)
     return (
       <div className="flex items-center justify-center h-screen text-blue-600">
         Loading team details...
@@ -65,107 +92,85 @@ const TeamDetails = () => {
               </i>
             </div>
             <div>
-              <h3 className="text-xl font-bold text-white-900 mb-4">
+              <h3 className="text-xl font-bold text-white-900 mb-4 ">
                 Team Members:
               </h3>
-              <ul className="space-y-3">
+              <ul className="space-y-3 overflow-y-auto max-h-[50vh]">
                 {team.members.map((member) => (
                   <li
                     key={member._id}
-                    className="flex items-center justify-between p-4 bg-white/5  backdrop-opacity-5 backdrop-brightness-10 shadow-lg backdrop-blur-sm font-bold text-xl rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+                    className="flex flex-col md:flex-row md:justify-between gap-4  p-4 bg-white/5  backdrop-opacity-5 backdrop-brightness-10 shadow-lg backdrop-blur-sm font-bold text-xl rounded-lg transform transition-transform duration-300 hover:shadow-lg"
                   >
-                    <div className="block md:flex justify-center md:justify-end items-center">
-                      <div className="flex flex-col">
-                        <span className="font-bold text-blue-600">
-                          {member.name}
-                        </span>
-                        <span className="text-gray-500 font-semibold text-sm">
-                          Email: {member.email}
-                        </span>
-                        <span className="text-gray-600 font-normal text-xs">
-                          Branch: {member.branch}
-                        </span>
-                        <span className="text-gray-600 font-normal text-xs">
-                          College: {member.collegeName}
-                        </span>
-                        <span className="text-gray-600 font-normal text-xs">
-                          Roll No: {member.rollNo}
-                        </span>
-                      </div>
-                      <div className="md:hidden flex justify-center items-center mt-4 md:justify-end">
-                        {user.isLeader && user.rollNo != member.rollNo &&
-                          <button
-                            className="bg-red-500 hover:bg-red-600 rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2"
-
-                            // className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md font-bold text-sm"
-                            onClick={() => handleDelete(member.teamId, member._id)}
-                          >
-                            Delete Member
-                          </button>
-
-                        }
-                      </div>
-                      {/* Additional member-specific actions can be added here */}
+                    <div className="flex flex-col items-start justify-start">
+                      <span className="font-bold text-blue-600">
+                        {member.name}
+                      </span>
+                      <span className="text-gray-500 font-semibold text-sm">
+                        Email: {member.email}
+                      </span>
+                      <span className="text-gray-600 font-normal text-xs">
+                        Branch: {member.branch}
+                      </span>
+                      <span className="text-gray-600 font-normal text-xs">
+                        College: {member.collegeName}
+                      </span>
+                      <span className="text-gray-600 font-normal text-xs">
+                        Roll No: {member.rollNo}
+                      </span>
                     </div>
-                    <div className="md:flex hidden justify-center items-center mt-4 md:justify-end">
-                      {user.isLeader && user.rollNo != member.rollNo &&
+
+                    <div className=" self-start md:self-end items-center">
+                      {user.isLeader && user.rollNo != member.rollNo && (
+                        <button
+                          className="bg-red-500 hover:bg-red-600 rounded-md text-xs md:text-base  font-semibold px-4 sm:px-1 py-2 md:px-4 md:py-2"
+                          // className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md font-bold text-sm"
+                          onClick={() => handleDeleteMember(member._id)}
+                        >
+                          Remove
+                        </button>
+                      )}
+                    </div>
+                    {/* <div className="md:flex hidden justify-center items-center mt-4 md:justify-end">
+                      {user.isLeader && user.rollNo != member.rollNo && (
                         <button
                           className="bg-red-500 hover:bg-red-600 rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2"
-
                           // className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md font-bold text-sm"
-                          onClick={() => handleDelete(member.teamId, member._id)}
+                          onClick={() => handleDeleteMember(member._id)}
                         >
-                          Delete Member
+                          Remove
                         </button>
-
-                      }
-                    </div>
+                      )}
+                    </div> */}
                   </li>
                 ))}
               </ul>
-              <div className="md:flex hidden justify-center items-center mt-4 md:justify-end"></div>
-              {user.isLeader &&
-                <>
-                  
-                    {team.members.length <=3 ?
-                    <Link
-                    to="/workshop/createteam"
-                  >
+              <div className="flex justify-start items-center mt-10">
+                {user.isLeader && (
+                  <>
+                    {team.members.length <= 3 ? (
+                      <Link to="/workshop/createteam">
+                        <button className="bg-green-500 hover:bg-green-600 rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2">
+                          Add Members
+                        </button>
+                      </Link>
+                    ) : (
                       <button
-                      className="bg-green-500 hover:bg-green-600 rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2"
+                        className="bg-gray-700  rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2"
+                        onClick={() => alert("Team is already Complete")}
+                      >
+                        Add Members
+                      </button>
+                    )}
 
-                      // className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md font-bold text-sm"
-                      onClick={() => handleDelete(member.teamId, member._id)}
-                    >
-                      Add Members
-                    </button>
-                    </Link>
-                     :
                     <button
-                    className="bg-gray-700  rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2"
-
-                    // className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md font-bold text-sm"
-                    onClick={() => alert("Team is already Complete")}
-                  >
-                    Add Members
-                  </button>
-                  }
-                  {/* <Link
-                    to="/workshop"
-                  > */}
-
-                  <button
-                    className="bg-red-500 hover:bg-red-600 rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2 mb-4"
-                    
-                    // onClick={() => handleDeleteTeam(user?._id)}
+                      className="bg-red-500 hover:bg-red-600 rounded-md text-xs md:text-base  font-semibold mx-1 md:mx-4 px-4 sm:px-1 py-2 md:px-4 md:py-2"
+                      onClick={handleDeleteTeam}
                     >
-                    Delete Team
-                  </button>
-                    {/* </Link> */}
-
-                </>
-
-              }
+                      Delete Team
+                    </button>
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </>
@@ -193,14 +198,12 @@ const TeamDetails = () => {
         </div>
       )}
       {message && (
-        <p className="font-mono text-sm mt-4 text-center text-green-500">
-          {message}
+        <p className="font-mono text-sm text-center text-green-500">
+          {loading ? "Removing..." : message}
         </p>
       )}
       {error && (
-        <p className="font-mono text-sm mt-4 text-center text-red-500">
-          {error}
-        </p>
+        <p className="font-mono text-sm text-center text-red-500">{error}</p>
       )}
     </div>
   );
