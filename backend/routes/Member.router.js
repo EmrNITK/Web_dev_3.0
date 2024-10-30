e02994ae801b5c0efe782ee7664d1e318e97498d
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-10-31 01:52:42 +0530

    Add put route

--- backend/routes/Member.router.js
+++ backend/routes/Member.router.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import removeMember from '../controllers/Member.controller.js';
+import {removeMember,addMember} from '../controllers/Member.controller.js';
 import { verifyJwt } from '../middlewares/auth.middlewares.js';
 
 const memberRouter = express.Router();
 
 memberRouter.delete('/:teamId/members/:memberId',verifyJwt, removeMember);
+memberRouter.put('/:teamId/members/:memberId',verifyJwt, addMember);
 
 export default memberRouter;
