bc546f9221c39dffbce2d30be4d0ceb45d4e83ab
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-12-18 19:58:44 +0530

    Remove 'branch' validation & Add 'kaggleUserName' validation

--- backend/middlewares/validate.middlewares.js
+++ backend/middlewares/validate.middlewares.js
@@ -5,9 +5,10 @@ export const validateRegistration = [
   body("email").isEmail(),
   body("rollno").escape().trim(),
   body("password").escape().trim(),
-  body("branch").escape().trim(),
+  // body("branch").escape().trim(),
   body("mobileNo").escape().trim(),
   body("collegeName").escape().trim(),
+  body("kaggleUserName").escape().trim(),
   (req, res, next) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
