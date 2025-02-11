ee202b01e183932d11e6eb080cb26d3b665a6f05
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2025-02-11 16:38:57 +0530

    feat: Change ListManager.jsx to DynamicList.jsx and Add functionality to edit each entry

---
+++ frontend/src/components/DynamicList.jsx
@@ -0,0 +1,129 @@
+import React, { useContext, useState } from "react";
+import { EventFormContext } from "../context/EventFormContext";
+import useUpdateEffect from "../hooks/useUpdateEffect";
+import { FaTrash, FaEdit, FaCheck } from "react-icons/fa";
+import { Input } from "./Input";
+
+export const DynamicList = ({ fields, section, isEditing }) => {
+  const { eventData, updateSection } = useContext(EventFormContext);
+  const [items, setItems] = useState(eventData[section]);
+  const [inputValues, setInputValues] = useState(fields.map(() => ""));
+
+  useUpdateEffect(() => {
+    console.log('render');
+    updateSection(section, items);
+  }, [isEditing]);
+
+  const handleAdd = () => {
+    if (inputValues.every((val) => val.trim())) {
+      const newItem = fields.reduce((obj, field, index) => {
+        obj[field.name] = inputValues[index];
+        return obj;
+      }, {});
+      setItems([...items, newItem]);
+      setInputValues(fields.map(() => ""));
+    }
+  };
+  //   console.log(inputValues);
+
+  const handleDeleteItem = (index) => {
+    setItems(items.filter((_, i) => i !== index));
+  };
+
+  const handleEditItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems[index].editing = true; // Add editing state
+    setItems(updatedItems);
+  };
+
+  const handleSaveEdit = (index) => {
+    const updatedItems = [...items];
+    delete updatedItems[index].editing; // Remove editing state after saving
+    setItems(updatedItems);
+  };
+
+  const handleInputChange = (e, index, field) => {
+    const updatedItems = [...items];
+    updatedItems[index][field] = e.target.value;
+    setItems(updatedItems);
+  };
+  return (
+    <>
+      {isEditing ? (
+        <div className="grid grid-cols-[3fr_3fr_.75fr] gap-4 align-center">
+          {fields.map((field, index) => {
+            return (
+              <Input
+                key={`${field.name}-${index}`}
+                label={field.label}
+                disabled={!isEditing}
+                id={`${field.name}-${index}`}
+                type={field.type}
+                value={inputValues[index]}
+                onChange={(e) => {
+                  const updatedInputValues = [...inputValues];
+                  updatedInputValues[index] = e.target.value;
+                  setInputValues(updatedInputValues);
+                }}
+              />
+            );
+          })}
+
+          <button
+            onClick={handleAdd}
+            className="text-sm justify-self-end self-center px-2 py-1 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-md"
+          >
+            Add
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      {items.length > 0 && (
+        <div className="bg-gray-800 p-3 gap rounded-md mt-3">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center gap-2 bg-gray-700 p-2 rounded-md my-1"
+            >
+              {fields.map((field) => (
+                <Input
+                  label={field.label}
+                  key={`${field.name}-${index}`}
+                  id={`${field.name}-${index}`}
+                  name={field.name}
+                  type={field.type}
+                  value={item[field.name] || ""}
+                  disabled={!isEditing || !item.editing}
+                  onChange={(e) => handleInputChange(e, index, field.name)}
+                />
+              ))}
+
+              {/* Controls: Edit, Save, Delete */}
+              {isEditing && (
+                <div className="flex gap-4">
+                  {item.editing ? (
+                    <FaCheck
+                      className="text-green-500 hover:text-green-700 cursor-pointer"
+                      onClick={() => handleSaveEdit(index)}
+                    />
+                  ) : (
+                    <FaEdit
+                      className="text-yellow-500 hover:text-yellow-700 cursor-pointer"
+                      onClick={() => handleEditItem(index)}
+                    />
+                  )}
+                  <FaTrash
+                    className="text-red-500 hover:text-red-700 cursor-pointer"
+                    onClick={() => handleDeleteItem(index)}
+                  />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
