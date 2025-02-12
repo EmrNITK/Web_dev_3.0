b27506ce7395320a1dd9b88d83a9b58c30d86f6e
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2025-02-12 17:29:04 +0530

    feat: Add middleware to process base64 files

--- backend/routes/Event.router.js
+++ backend/routes/Event.router.js
@@ -1,27 +1,38 @@
-import express from 'express';
-import { createEvent , getEventById , updateEventById , deleteEventById , getAllEvent } from '../controllers/Event.controller.js';
-import { upload } from '../middlewares/multer.middleware.js';
-import { verifyJwt } from '../middlewares/auth.middlewares.js';
-import { validateCreateEvent } from '../middlewares/validate.middlewares.js';
+import express from "express";
+import {
+  createEvent,
+  getEventById,
+  updateEventById,
+  deleteEventById,
+  getAllEvent,
+} from "../controllers/Event.controller.js";
+import {
+  upload,
+  processBase64Files,
+} from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middlewares.js";
+import { validateCreateEvent } from "../middlewares/validate.middlewares.js";
 
 const eventRouter = express.Router();
 
+eventRouter.post(
+  "/",
+  processBase64Files([
+    { name: "poster", filename: "poster.jpg" }, // Required
+    { name: "qrCode", filename: "qrCode.jpg" }, // Optional
+  ]),
+  upload.fields([
+    { name: "poster", maxCount: 1 },
+    { name: "qrCode", maxCount: 1 },
+  ]), // Multer will now process the newly created files
+  validateCreateEvent,
+  verifyJwt,
+  createEvent
+);
 
-eventRouter.post('/',
-    upload.fields([      // middleware
-        {
-            name: "poster",
-            maxCount: 1
-        },
-        {
-            name: "qrCode",
-            maxCount: 1
-        }
-]),validateCreateEvent,verifyJwt,createEvent)
-
-eventRouter.get('/',verifyJwt,getAllEvent)
-eventRouter.get('/:eventId',verifyJwt,getEventById)
-eventRouter.put('/:eventId',verifyJwt,updateEventById)
-eventRouter.delete('/:eventId',verifyJwt,deleteEventById)
+eventRouter.get("/", verifyJwt, getAllEvent);
+eventRouter.get("/:eventId", verifyJwt, getEventById);
+eventRouter.put("/:eventId", verifyJwt, updateEventById);
+eventRouter.delete("/:eventId", verifyJwt, deleteEventById);
 
 export default eventRouter;
