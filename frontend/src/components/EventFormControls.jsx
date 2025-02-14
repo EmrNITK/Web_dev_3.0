f203b481f835275c442467a93f02e20a0897fad5
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2025-02-14 19:00:48 +0530

    feat: Implement handlePublishing method

--- frontend/src/components/EventFormControls.jsx
+++ frontend/src/components/EventFormControls.jsx
@@ -5,15 +5,16 @@ import { useNavigate } from "react-router-dom";
 import { useApiRequest } from "@/hooks/useApiRequest.jsx";
 
 export const EventFormControls = ({ disabled }) => {
-  const { eventData } = useContext(EventFormContext);
+  const { eventData, updatedValues } = useContext(EventFormContext);
   const [isLive, setIsLive] = useState(eventData.isLive);
-  const { request, loading } = useApiRequest();
+  const { request, loading } = useApiRequest({ enableToast: true });
   const navigate = useNavigate();
 
   const handleSave = async () => {
+    const { _id, ...data } = eventData;
     const apiCall = disabled
-      ? () => updateEvent(eventData)
-      : () => createEvent(eventData);
+      ? () => updateEvent(eventData._id, updatedValues)
+      : () => createEvent(data);
 
     const successMessage = disabled
       ? "Event updated successfully!"
@@ -23,21 +24,45 @@ export const EventFormControls = ({ disabled }) => {
     if (response) navigate("/events/manage");
   };
 
-  const handlePublishing = () => {};
+  const handlePublishing = async () => {
+    const { _id, ...data } = eventData;
+    const apiCall = eventData._id
+      ? eventData.isLive
+        ? () =>
+            updateEvent(eventData._id, {
+              ...updatedValues,
+              isLive: false,
+            })
+        : () =>
+            updateEvent(eventData._id, {
+              ...updatedValues,
+              isLive: true,
+            })
+      : () => createEvent({ ...data, isLive: true });
+
+    const successMessage = eventData.isLive
+      ? "Event unpublished succesfully!"
+      : "Event published successfully!";
+
+    const response = await request(apiCall, successMessage);
+
+    if (response) navigate("/events/manage");
+  };
   return (
     <>
-      <div className="flex space-x-4">
+      <div className="flex space-x-4 items-center gap-4">
+        {loading ? (
+          <div className="font-mono text-green-400"> Processing...</div>
+        ) : (
+          ""
+        )}
         <button
           className="px-5 py-2 bg-green-600 active:bg-green-900 
              text-gray-300 active:text-gray-500 font-medium rounded-md border border-gray-600 
              shadow-sm hover:shadow-md "
           onClick={handleSave}
         >
-          {loading
-            ? "Processing..."
-            : disabled
-            ? "Update Event"
-            : "Create Event"}
+          {disabled ? "Update Event" : "Create Event"}
         </button>
 
         <button
@@ -46,7 +71,7 @@ export const EventFormControls = ({ disabled }) => {
              text-gray-300 font-medium rounded-md border border-gray-600 
              shadow-sm active:shadow-md transition"
         >
-          { isLive ? "Unpublish" : "Publish"}
+          {isLive ? "Unpublish" : "Publish"}
         </button>
       </div>
     </>
