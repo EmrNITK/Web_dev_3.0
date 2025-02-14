cf708b97a4b30ab4bc67ddb40831e7e97f880e7a
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2025-02-14 17:17:02 +0530

    feat: Setup project to use shadcn/ui

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,26 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
-    extend: {
-      colors: {
-        primary: "#050816",
-        primaryy: "#050816b8",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
-      },
-      boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
-      },
-      screens: {
-        xs: "450px",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: '#050816',
+  			primaryy: '#050816b8',
+  			secondary: '#aaa6c3',
+  			tertiary: '#151030',
+  			'black-100': '#100d25',
+  			'black-200': '#090325',
+  			'white-100': '#f3f3f3'
+  		},
+  		boxShadow: {
+  			card: '0px 35px 120px -15px #211e35'
+  		},
+  		screens: {
+  			xs: '450px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
 
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
