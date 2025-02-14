5e9bfb1573f34f9c2e0f7d5ac16867eff5635e76
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2025-02-14 19:01:23 +0530

    feat: Update to fetch events from backend

--- frontend/src/pages/ManageEventsPage.jsx
+++ frontend/src/pages/ManageEventsPage.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { events } from "../utils/data";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PageLayout from "../components/PageLayout";
 import EventsSection from "../components/EventSection";
+import { useApiRequest } from "@/hooks/useApiRequest";
+import { getAllEvents } from "@/api/apiService";
 
 export default function ManageEventsPage() {
-  console.log(events);
   const navigate = useNavigate();
+  const [events, setEvents] = useState([]);
+  const { request, loading } = useApiRequest({ enableToast: false });
+
+  useEffect(() => {
+    async function fetch() {
+      const response = await request(getAllEvents, "");
+      console.log(response);
+      setEvents(response.events);
+    }
+
+    fetch();
+  }, []);
+
   return (
     <PageLayout title={"Manage Events"}>
       <EventsSection
